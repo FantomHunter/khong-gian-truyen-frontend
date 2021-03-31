19c280737a4cc4db8657f5b38685a30453dc1c8c
codehunter <hunterx6323@gmail.com>
2021-03-31 21:47:33 +0700

    reative header when routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'khong-gian-truyen-frontend';
+  constructor(private store: Store){
+
+  }
 }
