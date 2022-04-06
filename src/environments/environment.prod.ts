7858d54c2c16005227ab137526ac48b054e41e6f
codehunter <hunterx6323@gmail.com>
2022-04-06 20:33:07 +0700

    update prod config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,30 @@
 export const environment = {
-  production: true
+  production: true,
+  backendUrl: '${BACKEND_URL}',
+  debug: true,
+  useMockService: false,
+  features: {
+    productDetail: {
+      comment: {
+        size: 5
+      }
+    },
+    productPaging: {
+      size: 9
+    },
+    popularProduct: {
+      isEnable: false
+    },
+    topNewProduct: {
+      isEnable: false
+    },
+    topViewProduct: {
+      isEnable: false
+    },
+    sidebar: {
+      topViewProduct: {
+        isEnable: false
+      }
+    }
+  }
 };
